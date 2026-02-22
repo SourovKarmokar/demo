45d130bf59d73056c14a794728751d265ba0199c
SourovKarmokar <sourovkarmokar22@gmail.com>
2026-02-22 22:33:18 +0600

    ListItem

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ListItem from "./components/ListItem";
+
+
+export default function App() {
+  
+
+  return (
+    <div className="App">
+      <ListItem/>
+    </div>
+  );
+}
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,16 @@
+export default function ListItem() {
+    return (
+        <div>
+            <div className="row">
+                <img src="https://image-placeholder.com/images/actual-size/75x100.png" alt="Itemname" />
+            </div>
+            <div className="content">
+                <div>Item name</div>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quisquam?
+                </div>
+
+            </div>
+        </div>
+    );
+}
